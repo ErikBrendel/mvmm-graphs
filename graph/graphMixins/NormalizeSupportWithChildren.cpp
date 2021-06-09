547dafce1e33b0908b2eaf4bf22ba547cb9f9e5f
Erik Brendel <erik.brendel@gmx.de>
2021-06-09 21:51:09 +0200

    debug

--- graph/graphMixins/NormalizeSupportWithChildren.cpp
+++ graph/graphMixins/NormalizeSupportWithChildren.cpp
@@ -1,7 +1,9 @@
+#include <iostream>
 #include "NormalizeSupportWithChildren.h"
 
 float NormalizeSupportWithChildren::getAbsoluteSupport(const string& node) {
     float result = getAbsoluteSelfSupport(node);
+    std::cout << "getAbsoluteSupport " << node << endl;
     for (const auto& child: getChildren(node)) {
         result += getAbsoluteSupport(child);
     }
