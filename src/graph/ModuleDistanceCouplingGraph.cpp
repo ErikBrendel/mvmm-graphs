9cac379595382bb49f7e7a8dff1e4236d3fa8843
Erik Brendel <erik.brendel@student.hpi.de>
2021-09-02 18:46:49 +0200

    adjust module distance metric to return a coupling of 1 for sibling - modules or direct parent-child modules (those that have a path distance of 1)

--- src/graph/ModuleDistanceCouplingGraph.cpp
+++ src/graph/ModuleDistanceCouplingGraph.cpp
@@ -12,9 +12,11 @@ int path_module_distance(const string& a, const string& b) {
     int minLen = (int)min(steps_a.size(), steps_b.size());
     int totalLength = (int)(steps_a.size() + steps_b.size());
     rep(i, minLen) {
-        if (steps_a[i] == steps_b[i]) continue;
-        //unequal: calc distance
-        return totalLength - (int)(i * 2);
+        if (steps_a[i] != steps_b[i]) {
+            // unequal: calc distance
+            // one is subtracted to allow a "direct step" from one node in the tree to its sibling
+            return totalLength - (int) (i * 2) - 1;
+        }
     }
     return totalLength - (minLen * 2);
 }
@@ -27,6 +29,8 @@ double ModuleDistanceCouplingGraph::getNormalizedSupport(const string& node) {
 
 double ModuleDistanceCouplingGraph::getNormalizedCoupling(const string& a, const string& b) {
     int dist = path_module_distance(a, b);
+    // one is subtracted from the dist, so that the lowest possible dist (1) a perfect coupling score
+    if (dist > 0) dist--;
     double base = 1.1f;
     return (double) pow(base, -dist);
 }
