b49abb60db3e366faa559a102fcef29d641e8606
Erik Brendel <erik.brendel@student.hpi.de>
2021-11-20 16:12:09 +0100

    add debug infos to production build

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 # pull latest changes and build the new executable, directly replacing any old build
-git pull && cmake . -DCMAKE_BUILD_TYPE=Release && make -j
+git pull && cmake . -DCMAKE_BUILD_TYPE=RelWithDebInfo && make -j
