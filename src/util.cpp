71d4127dfaf22670562a15929080bdd8507ef4ae
Erik Brendel <erik.brendel@student.hpi.de>
2021-11-20 16:27:21 +0100

    adjust split implementation to maybe not crash anymore

--- src/util.cpp
+++ src/util.cpp
@@ -16,14 +16,14 @@ std::vector<std::string> split(const std::string &s, char delim) {
 
 std::vector<std::string> &split(const std::string &s, const std::string& delims, std::vector<std::string> &elems) {
     char* tok;
-    char cchars [s.size()+1];
-    char* cstr = &cchars[0];
-    strcpy(cstr, s.c_str());
+    char* cstr = new char[s.size() + 1];
+    memcpy(cstr, s.c_str(), s.size() + 1);
     tok = strtok(cstr, delims.c_str());
     while (tok != nullptr) {
         elems.emplace_back(tok);
         tok = strtok(nullptr, delims.c_str());
     }
+    delete[] cstr;
     return elems;
 }
 
