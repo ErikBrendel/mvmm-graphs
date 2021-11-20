090a026adc1865ef06a5fa34533c8b53c30e512d
Erik Brendel <erik.brendel@student.hpi.de>
2021-11-20 15:51:19 +0100

    better SIGSEGV handling

--- src/errorHandler.cpp
+++ src/errorHandler.cpp
@@ -3,11 +3,13 @@
 #include <csignal>
 #include <cstdlib>
 #include <unistd.h>
+#include <iostream>
 
 template <typename T>
 void errorSignalHandler(T sig) {
+    std::cout << "Exiting due to segmentation fault..." << std::endl;
     void *array[10];
-    size_t size;
+    int size;
 
     // get void*'s for all entries on the stack
     size = backtrace(array, 10);
@@ -15,7 +17,7 @@ void errorSignalHandler(T sig) {
     // print out all the frames to stderr
     fprintf(stderr, "Error: signal %d:\n", sig);
     backtrace_symbols_fd(array, size, STDERR_FILENO);
-    exit(1);
+    exit(sig);
 }
 
 #define installErrorHandler signal(SIGSEGV, errorSignalHandler<int>)
